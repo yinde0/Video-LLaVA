6c220e48973fd1ef1212de9900bbabe6de84c141
Saad Salami <35911370+yinde0@users.noreply.github.com>
2025-07-19 15:31:25 +0000

    Update constants.py

--- videollava/constants.py
+++ videollava/constants.py
@@ -7,6 +7,7 @@ LOGDIR = "."
 IGNORE_INDEX = -100
 
 IMAGE_TOKEN_INDEX = -200
+SPATIO_TOKEN_INDEX = -201
 DEFAULT_IMAGE_TOKEN = "<image>"
 DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
 DEFAULT_IM_START_TOKEN = "<im_start>"
@@ -19,6 +20,7 @@ DEFAULT_VIDEO_PATCH_TOKEN = "<im_patch>"
 DEFAULT_VID_START_TOKEN = "<vid_start>"
 DEFAULT_VID_END_TOKEN = "<vid_end>"
 VIDEO_PLACEHOLDER = "<video-placeholder>"
+DEFAULT_SPATIO_TOKEN = "<spatio>"
 # ======================================================================================================
 
 MAX_IMAGE_LENGTH = 16
